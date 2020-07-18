35a482b2ee0ebb10466c1a8e440f270688b97825
Manoj <Manoj.Maduri@mckesson.com>
2020-07-18 17:42:39 -0400

    expired variables removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDq3GrnNk1j__0QYhJBY1lFPAnanNAA3_Q",
-    authDomain: "ng-modell.firebaseapp.com",
-    databaseURL: "https://ng-modell.firebaseio.com",
-    projectId: "ng-modell",
-    storageBucket: "ng-modell.appspot.com",
-    messagingSenderId: "866444622635",
-    appId: "1:866444622635:web:26eebb3ab1778d2c89236e",
-    measurementId: "G-0X4DFLVTZX"
-  },
-  apiKey: "8cde955cc1d449f2864ba717eae1e219"
+  production: true
 };
